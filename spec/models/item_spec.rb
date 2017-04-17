c9a72a334ace7209cf6f421340cb870f03e848fc
Alex Rapley <rapley3@gmail.com>
2017-04-17 18:12:59 +1000

    Update item spec tests to use expect syntax

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 describe Item do
   it "has a valid factory" do
-    FactoryGirl.create(:item).should be_valid
+    factory = FactoryGirl.create(:item)
+    expect(factory).to be_valid
   end
 
   it "is invalid without a title" do
-    FactoryGirl.build(:item, title: nil).should_not be_valid
+    item = FactoryGirl.build(:item, title: nil)
+    expect(item).not_to be_valid
   end
 
   it "is invalid without a description" do
-    FactoryGirl.build(:item, description: nil).should_not be_valid
+    item = FactoryGirl.build(:item, description: nil)
+    expect(item).not_to be_valid
   end
 end
