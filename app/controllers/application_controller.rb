0578880bba1ace7217f32ca31245fa0fa3c87eb8
Alex Rapley <rapley3@gmail.com>
2017-04-18 14:04:29 +1000

    Finish implementing user types with radio buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:is_informant])
   end
 end
