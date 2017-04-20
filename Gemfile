2fd70f82374cbc0eed08ee0810dbdb375f66872d
Alex Rapley <rapley3@gmail.com>
2017-04-20 11:42:57 +1000

    Remove migration file

--- Gemfile
+++ Gemfile
@@ -30,7 +30,6 @@ end
 group :test do
   gem 'faker'
   gem 'capybara'
-  gem 'guard-rspec'
   gem 'launchy'
 end
 
--- db/migrate/20170419130133_remove_requested_from_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRequestedFromItems < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :items, :requested, :boolean
-  end
-end
