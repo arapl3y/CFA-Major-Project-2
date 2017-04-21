21a2634a7cbe6bee2f7df99830ca4c44ee2e0d4e
Alex Rapley <rapley3@gmail.com>
2017-04-21 20:22:37 +1000

    Add JS for flash fade out and fixed invalid form html in informant table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).on('turbolinks:load', function(){
+  $('.alert').delay(1000).fadeOut(4000);
+});
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :item_requests
-  # requested items is an arbibrary name to find items that the user has requested more easily
+
+  # requested items is an arbitrary name to find items that the user has requested more easily
   has_many :requested_items, through: :item_requests, source: :item
 
   def email_required?
