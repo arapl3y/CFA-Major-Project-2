23b5b6c593a18ff30546f90c3552908dfcb8097e
Alex Rapley <rapley3@gmail.com>
2017-04-24 21:17:02 +1000

    Finish styling plans table

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,11 @@ class ItemsController < ApplicationController
 
   def home
     @basic_plan = Plan.find(1)
-    @premium_plan = Plan.find(2)
   end
 
   def plans
-
+    @basic_plan = Plan.find(1)
+    @premium_plan = Plan.find(2)
   end
 
   def dashboard
