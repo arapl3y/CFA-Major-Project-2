f90d4614bf9604cf1d216dde99029d1c82a43634
Alex Rapley <rapley3@gmail.com>
2017-04-25 15:22:02 +1000

    Add paperclip initializer file to change aws default url

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:url] = 'secretsourcecfa.s3-ap-southeast-2.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 'secretsourcecfa.s3-ap-southeast-2.amazonaws.com'
