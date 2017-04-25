cc97eab97e8409597f29b7e5b52d83b59d8bbd7b
Alex Rapley <rapley3@gmail.com>
2017-04-25 16:18:27 +1000

    Debug issue with aws on localhost

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,13 @@ class Item < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
 
-  has_attached_file :document
+  has_attached_file :document,
+    storage: :s3,
+    bucket: ENV['S3_BUCKET_NAME'],
+    s3_credentials: {
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
 
   # Permitted file types
   validates_attachment :document, :content_type => {:content_type => %w(image/jpeg image/jpg image/png image/gif image/svg application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document audio/mpeg audio/mp3 audio/x-m4a)}
