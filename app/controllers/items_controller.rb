4bb10eb42354f061369b3f04a6e1c50629ccf6d5
Alex Rapley <rapley3@gmail.com>
2017-04-25 10:19:55 +1000

    Add user authentication for all pages except home and plans

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :find_item, only: [:show, :edit, :update, :destroy]
-  # before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:home, :plans]
 
   def index
     @search = Item.search(params[:q])
